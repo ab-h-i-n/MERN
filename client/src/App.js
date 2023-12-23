5572a4d335bb33c6f9556b0eefd8fdfdb4f7e1ee
ab-h-i-n <abhinshelly@gmail.com>
2023-12-23 17:34:12 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'>
+    <form>
+    <h1 className='App-logo'>Register</h1>
+    <input type='text' placeholder='Name'></input>
+    <input type='email' placeholder='Email'></input>
+    <input type='password' placeholder='Password'></input>
+    <button type='submit'>Submit</button>
+    </form>
+  </div>
 }
 
 export default App;
